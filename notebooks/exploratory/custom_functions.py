18825c0418c48b5c3896e41a7575218fe1917827
Seth Schober <seth.schober@gmail.com>
2021-02-23 20:36:40 -0900

    Additional progress isolating residential properties. Also separated 'lookups' function into custom_functions.py file

--- notebooks/exploratory/custom_functions.py
+++ notebooks/exploratory/custom_functions.py
@@ -1,7 +1,11 @@
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
 
+# One-hot encode data coming in as a series
+# Optional prefix added to each column name
+# Optionally input list of columns to use instead ('name_lookup')
 def one_hot(srs, prefix='x', name_lookup = False):
-    from sklearn.preprocessing import OneHotEncoder
+    #from sklearn.preprocessing import OneHotEncoder
     #import pandas as pd
     
     ohe = OneHotEncoder(sparse=False, drop='first')
@@ -23,3 +27,13 @@ def one_hot(srs, prefix='x', name_lookup = False):
         df[col] = df[col].astype('int')
     
     return df
+
+
+# Extract definitions of encoded naming schemes when given a lookup code
+def get_lookups(LUType, df_lookup):
+    LUType = str(LUType)
+    
+    category = df_lookup.loc[df_lookup['LUType'] == LUType].copy()
+    category = category.sort_values(by='LUItem')
+    result = dict(zip(category.LUItem.str.strip(), category.LUDescription))
+    return result
